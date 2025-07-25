518e93c480862b24c3b2db436c303b760c0433a7
davirodrigues670 <davirodrigues240503@gmail.com>
2025-07-25 18:09:25 -0300

    instalei os pacotes

--- obj/Debug/net9.0/to-do-michelin.AssemblyInfo.cs
+++ obj/Debug/net9.0/to-do-michelin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("to-do-michelin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a973deb571bf73195d49099cd129ed5d36a29728")]
 [assembly: System.Reflection.AssemblyProductAttribute("to-do-michelin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("to-do-michelin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
